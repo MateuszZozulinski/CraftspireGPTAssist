f9a858ce705cc33d46f7ca8de633789479abfb8a
Mateusz Zozuliński <mateusz.zozulinski@craftspire.it>
2023-04-14 14:52:27 +0200

    Initial commit
    Signed-off-by: Mateusz Zozuliński <mateusz.zozulinski@craftspire.it>

---
+++ src/main/kotlin/it/craftspire/gptreview/GetReviewAction.kt
@@ -0,0 +1,40 @@
+package it.craftspire.gptreview
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.theokanning.openai.completion.chat.ChatCompletionRequest
+import com.theokanning.openai.completion.chat.ChatMessage
+import com.theokanning.openai.service.OpenAiService
+import java.time.Duration
+
+
+class GetReviewAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val file = e.getData(CommonDataKeys.PSI_FILE)
+        val lang = file!!.language
+        val editor = e.getRequiredData(CommonDataKeys.EDITOR)
+        val caretModel = editor.caretModel
+        val selectedText = caretModel.currentCaret.selectedText
+
+        val service = OpenAiService("token", Duration.ofSeconds(60))
+
+        System.out.println(lang);
+        val completionRequest = ChatCompletionRequest.builder()
+                .temperature(0.3)
+                .maxTokens(2048)
+                .messages(listOf(ChatMessage(
+                        "system", "You are an AI designed to do a brief code review, outlining most important issues in code provided in $lang "),
+                        ChatMessage("user", "below is code fragment, please provide me a brief code review, " +
+                                "each response line should single issue in format [linenumber:severity] issue:  \n$selectedText")))
+                .model("gpt-3.5-turbo")
+                .build()
+        service.createChatCompletion(completionRequest).getChoices().forEach(System.out::println)
+    }
+
+    override fun update(e: AnActionEvent) {
+        val editor = e.getRequiredData(CommonDataKeys.EDITOR)
+        val caretModel = editor.getCaretModel()
+        e.presentation.isEnabledAndVisible = caretModel.currentCaret.hasSelection()
+    }
+}
