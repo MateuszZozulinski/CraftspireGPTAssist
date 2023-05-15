118079075b65038cdc1153f8df0e28bf37061394
Mateusz Zozuliński <mateusz.zozulinski@craftspire.it>
2023-05-15 11:40:19 +0200

    New welcome text in plugin tool window
    Signed-off-by: Mateusz Zozuliński <mateusz.zozulinski@craftspire.it>

--- src/main/kotlin/it/craftspire/gptassist/windows/GPTToolWindowFactory.kt
+++ src/main/kotlin/it/craftspire/gptassist/windows/GPTToolWindowFactory.kt
@@ -13,8 +13,16 @@ import javax.swing.JTextPane
 class GPTToolWindowFactory : ToolWindowFactory {
 
     companion object {
+        val text = StringBuilder("")
         val textPane = JTextPane()
         private val INITIAL_TEXT = "<br/><p>Welcome to GPT Code Assist plugin. Select a code and execute action, and the response will be displayed in this window</p>"
+        private val INITIAL_TEXT_SETUP = "<br/><div>" +
+                "<p>Welcome to GPT Code Assist plugin. Before using the plugin you have to set it up, following these simple steps</p>" +
+                "<ul>" +
+                "<li>Obtain an OpenAI API key from <a href=\"https://platform.openai.com/account/api-keys\">https://platform.openai.com/account/api-keys</a> or use existing one.</li>" +
+                "<li>Open the Intellij Settings Dialog, find GPT Code Assist tab and set up the key</li>" +
+                "<li>Select any block of code - a new option will be available in the right-click context menu</li>" +
+                "</ul>"
         private val STRIPE_TITLE = "GPT Code Assist"
     }
 
@@ -24,7 +32,7 @@ class GPTToolWindowFactory : ToolWindowFactory {
         val content = ContentFactory.getInstance().createContent(panel, "", false)
         textPane.contentType = "text/html"
         textPane.font = Font(null, 0, 32)
-        textPane.text = INITIAL_TEXT
+        textPane.text = if (StoredStateComponent.instance.savedState.keySet) INITIAL_TEXT_SETUP else INITIAL_TEXT
         panel.add(textPane)
         toolWindow.contentManager.addContent(content)
     }
